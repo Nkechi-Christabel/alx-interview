4b05533ca026c346f491a4c00c939dd2ec450f40
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-18 05:14:32 -0700

    Updated the method that determines if all the boxes can be opened.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+"""
+This module determines if all the boxes can be opened or not.
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened using the keys contained in them.
+
+    Parameters:
+    - boxes (list of lists): A list of lists where each inner list represents
+      the keys contained in a box.
+
+    The first box, boxes[0], is unlocked initially.
+
+    Returns:
+    - bool: True if all boxes can be opened using the keys contained in them,
+    otherwise False.
+
+    If the input list is empty, returns False.
+    """
     if not boxes:
         return False
 
