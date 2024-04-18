6b8db515174c236caaaeff014bef609ed9dd36b3
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-18 04:57:24 -0700

    Wrote a method that determines if all the boxes can be opened..

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    num_boxes = len(boxes)
+    keys = set(boxes[0])
+    visited = {0}
+
+    while keys:
+        new_keys = set()
+        for key in keys:
+            if key < num_boxes and key not in visited:
+                visited.add(key)
+                new_keys.update(boxes[key])
+        keys = new_keys - visited
+
+    return len(visited) == num_boxes
