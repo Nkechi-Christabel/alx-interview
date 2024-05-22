9ac472f4a412cce97da01446819568e21f39d2c4
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-22 09:18:30 -0700

    Made the code semistandard compliant.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,41 @@
 #!/usr/bin/node
 
-
-
 const request = require('request');
 
 if (process.argv.length !== 3) {
-    console.log("Usage: ./0-starwars_characters.js <Movie ID>");
-    process.exit(1);
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
 const movieId = process.argv[2];
 
 if (isNaN(movieId)) {
-    console.log("Movie ID must be a number");
-    process.exit(1);
+  console.log('Movie ID must be a number');
+  process.exit(1);
 }
 
 const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(movieUrl, { json: true }, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-        console.error(`Failed to fetch data: ${err || `Status code ${res.statusCode}`}`);
-        process.exit(1);
-    }
+  if (err || res.statusCode !== 200) {
+    console.error(`Failed to fetch data: ${err || `Status code ${res.statusCode}`}`);
+    process.exit(1);
+  }
 
-    const characters = body.characters;
-    const characterNames = Array(characters.length).fill(null);
+  const characters = body.characters;
+  const characterNames = Array(characters.length).fill(null);
 
-    let completedRequests = 0;
+  let completedRequests = 0;
 
-    characters.forEach((url, index) => {
-        request(url, { json: true }, (err, res, body) => {
-            if (!err && res.statusCode === 200) {
-                characterNames[index] = body.name;
-            }
-            completedRequests++;
-            if (completedRequests === characters.length) {
-                characterNames.forEach(name => console.log(name));
-            }
-        });
+  characters.forEach((url, index) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        characterNames[index] = body.name;
+      }
+      completedRequests++;
+      if (completedRequests === characters.length) {
+        characterNames.forEach(name => console.log(name));
+      }
     });
+  });
 });
